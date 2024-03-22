bec6c6914fbb9a9082b8eccb6dc71d8425aca255
Guilherme Santiago de Brito <guilhermesantiagodebritod.s@gmail.com>
2024-03-22 15:38:14 -0300

    first version

---
+++ Combustivel_calculator/App.js
@@ -0,0 +1,11 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { Styles } from "./src/styles/Stylesheet";
+
+export default function App() {
+  return (
+    <View style={Styles.container}>
+      <Text>Teste</Text>
+    </View>
+  );
+}
