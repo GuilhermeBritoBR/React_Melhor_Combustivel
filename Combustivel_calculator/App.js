fd034e939f49177a7d6df167f7560a14c858413a
Guilherme Santiago de Brito <guilhermesantiagodebritod.s@gmail.com>
2024-03-27 13:48:38 -0300

    Atualização do APP

--- Combustivel_calculator/App.js
+++ Combustivel_calculator/App.js
@@ -1,11 +1,11 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
 import { Styles } from "./src/styles/Stylesheet";
+import TextComponent from "./src/components/partials/TextComponent";
 
 export default function App() {
   return (
     <View style={Styles.container}>
-      <Text>Teste</Text>
+      <TextComponent />
     </View>
   );
 }
--- Combustivel_calculator/src/styles/Stylesheet.js
+++ Combustivel_calculator/src/styles/Stylesheet.js
@@ -6,5 +6,37 @@ export const Styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+    backgroundColor: "black",
+  },
+  TextInput: {
+    borderWidth: 3,
+    backgroundColor: "white",
+    borderColor: "gray",
+    padding: 10,
+    marginTop: 10,
+  },
+  imgLogo: {
+    height: 250,
+    width: 250,
+    resizeMode: "cover",
+    marginTop: 30,
+  },
+  titulo: {
+    color: "white",
+    fontSize: 30,
+    fontWeight: "bold",
+    marginTop: 35,
+  },
+  TextAlcool: {
+    color: "white",
+    marginTop: 20,
+  },
+  TextGasolina: {
+    color: "white",
+    marginTop: 20,
+  },
+  Touchable: {
+    fontSize: 20,
+    color: "red",
   },
 });
